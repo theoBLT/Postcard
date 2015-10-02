8f7443a2a91739b378fd0e958be519aa31c86f8f
Théo Blochet <tblochet@ikomobi.com>
2015-10-02 13:53:25 -0400

    first rev of project

--- bitfountain/ViewController.swift
+++ bitfountain/ViewController.swift
@@ -9,6 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    // IB Outlet refers to the element
+    // weak indicates that this is a weak pointer. Strong doesn't make sense here
+    // var means that it will be a variable
+    // our name will be our way to reference to the object. 
+    
+    @IBOutlet weak var enterNameTextFieled: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var mailButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +30,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        
+        messageLabel.hidden = false
+        messageLabel.text = enterMessageTextField.text
+        messageLabel.textColor = UIColor.redColor()
+        
+        
+        enterMessageTextField.text = ""
+        
+        enterMessageTextField.resignFirstResponder()
+        //Close the keyboard
+        
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+        // Set a new title for the Button
+        
+    }
 
 }
 
