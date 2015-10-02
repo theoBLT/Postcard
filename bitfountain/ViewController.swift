5634a9786310f5f7baa9ca68ece0cfff36b46462
TheÃÅo Blochet <tblochet@ikomobi.com>
2015-10-01 20:52:35 -0400

    Initial Commit

---
+++ bitfountain/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bitfountain
+//
+//  Created by Théo Blochet on 01/10/15.
+//  Copyright © 2015 theo incorporated. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
